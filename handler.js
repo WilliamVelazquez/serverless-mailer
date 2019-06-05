43cae69db3613048f6876fceb67a6fd5474a573b
William Velazquez <william.velazquez.a@gmail.com>
2019-06-04 19:57:05 -0500

    Send Email Configuration and Default Values

--- handler.js
+++ handler.js
@@ -11,30 +11,48 @@ const transporter = nodeMailer.createTransport({
   }
 });
 
+const today = () => {
+  var now = new Date();
+  var y = now.getFullYear();
+  var m = now.getMonth() + 1;
+  var d = now.getDate();
+  return ` ${y}-${m < 10 ? '0' : ''}-${m}-${d < 10 ? '0' : ''}-${d}`;
+}
+
 // module.exports.contact = (event, context, callback) => {
 module.exports.contact = (req, res) => {
-  console.log("req-->", req);
-  console.log("res-->", res);
+  // console.log("req-->", req);
+  // console.log("res-->", res);
   // console.log("callback-->", callback);
 
   const body = req.body;
   console.log("body-->", body);
   const mailOpts = {
-    from: body.from || `"Fred Foo 👻" <${process.env.CONTACT_ADDRESS}>`,
-    to: process.env.EMAIL_ADDRESS,
+    // from: body.from || `"Fred Foo 👻" <${process.env.CONTACT_ADDRESS}>`,
+    from: `${body.name ? body.name:'Contact Page'} 📧 <${process.env.CONTACT_ADDRESS}>`,
+    to: process.env.CONTACT_ADDRESS,
     //cc: [],
     //bcc: body.bcc || '',
-    subject: body.subject || '[No subject]' + Date.now(),
-    text: body.text || 'Test Text Body', // plain text body
-    html: body.message || '[No message]  <p><b>Hello<br/>World!</b></p>' // html body
+    subject: body.subject ? `${body.subject} - Contact Page`:'Contact Page', //+ Date.now().toString(),//millisecond
+    text: body.text || '[No text message]', // plain text body
+    html: `<div>
+            <p><b>Name: </b>${body.name||'-'}</p>
+            <p><b>E-mail: </b>${body.email||'-'}</p>
+            <p><b>Phone: </b>${body.phone||'-'}</p>
+            <p><b>Comments: </b>${body.comments||'-'}</p>
+            <p><b>Want a call? </b>${body.callBack?'Yes':'No'}</p>
+            <br>
+            <span style='text-align:center;'>${today()}</span>
+          </div>` 
+    || `[No message] 💻 ${today()}` // html body
   };
 
   // verify connection configuration
   transporter.verify(function(error, success) {
     if (error) {
-      console.log(error);
+      console.log("Connection Configuration Error-->", error);
     } else {
-      console.log("Server is ready to send messages");
+      // console.log("Server is ready to send messages");
     }
   });
 
